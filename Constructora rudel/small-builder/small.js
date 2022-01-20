281f10c94bc5908543dd0e9aa8e4ddf36afd9574
Programador Front-end <79943027+Francisco-Va@users.noreply.github.com>
2022-01-19 22:09:34 -0600

    Add files via upload

---
+++ Constructora rudel/small-builder/small.js
@@ -0,0 +1,8 @@
+// ******************script de las páginas de imagenes************
+document.addEventListener('DOMContentLoaded', () => {
+	const imgLightBox = document.querySelectorAll('.materialboxed');
+	M.Materialbox.init(imgLightBox, {
+		inDuration: 500,
+		outDuration: 500
+	});
+});
